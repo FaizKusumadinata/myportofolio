b9162626001d921c0293bf2e9d62a3ca96cd1021
Faiz Kusumadinata <faizkusumadinata12@gmail.com>
2026-09-11 08:36:57 +0700

    Finished two functional templates, added data, completed css, and added tests

--- README.md
+++ README.md
@@ -13,3 +13,14 @@ Tanggal Lahir: 6 November 2006
 
 AI Disclosure.
 Dalam pengerjaan Tugas-1 PBP ini, saya tidak menggunakan AI. Sumber utama saya dalam mempelajari materi untuk tugas ini adalah W3school.com dan StackOverflow.
+
+### tugas 2
+
+1. Ketika halaman portofolio, diakses, maka urls.py akan memanggil views.py untuk mencari fungsi yang sesuai request, kemudian fungsi tersebut akan mengambil data dari models.py dan menampilkannya pada templates.py yang sudah dikonfigurasikan padanya.
+
+2. Data pada portofolio disimpan dalam Model untuk memberi kemudahan ketika ingin kembali menciptakan data dengan atribut yang sama. Hal ini mempermudah developer dalam menciptakan data baru tanpa harus membuatnya dari awal lagi, mengikuti prinsip DRY(Don't Repeat Yourself).
+
+3. makemigrations membuat file yang berisi perubahan pada models.py, sedangkan migrations melakukan proses migrasi file tersebut ke database. Sebagai contoh, ketika seorang developer menambahkan atau menghapus atribut pada sebuah Class Model, atau menciptakan Class Model baru, dia harus melakukan makemigrations kemudian migrate.
+
+AI Disclosure.
+Dalam pengerjaan Tugas-2 PBP ini, saya tidak menggunakan AI. Sumber utama saya dalam mempelajari materi untuk tugas ini adalah W3school.com, geeksforgeeks.org, dan StackOverflow.
--- main/tests.py
+++ main/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from main.models import Experience
+from main.models import Experience, Artwork
 
 
 class MainTest(TestCase):
@@ -12,6 +12,12 @@ class MainTest(TestCase):
             description="Membantu mahasiswa memahami pengembangan web.",
             category="part-time",
         )
+        
+        self.artwork = Artwork.objects.create(
+            title="As Outward, so Inward",
+            description = "An artwork inspired by Hermetic Philosophy",
+            art_image = "/static/img/As Outward, so Inward.jpeg"
+        )
 
     def test_main_url_is_accessible(self):
         response = self.client.get(reverse("main:show_main"))
@@ -20,6 +26,8 @@ class MainTest(TestCase):
         self.assertTemplateUsed(response, "index.html")
         self.assertNotContains(response, self.experience.title)
         self.assertContains(response, f'href="{reverse("main:show_experience")}"')
+        self.assertNotContains(response, self.artwork.title)
+        self.assertContains(response, f'href="{reverse("main:show_artworks")}"')
 
     def test_nonexistent_page_returns_404(self):
         response = self.client.get("/halaman-yang-tidak-ada/")
@@ -30,6 +38,11 @@ class MainTest(TestCase):
         self.assertEqual(str(self.experience), "Asisten Dosen PBP")
         self.assertEqual(self.experience.category, "part-time")
         self.assertTrue(self.experience.is_ongoing)
+        
+    def test_artwork_model(self):
+        self.assertEqual(str(self.artwork), "As Outward, so Inward")
+        self.assertEqual(self.artwork.description, "An artwork inspired by Hermetic Philosophy")
+        self.assertEqual(self.artwork.art_image, "/static/img/As Outward, so Inward.jpeg")
 
     def test_experience_page(self):
         response = self.client.get(reverse("main:show_experience"))
@@ -45,8 +58,21 @@ class MainTest(TestCase):
     def test_empty_experience_page(self):
         Experience.objects.all().delete()
         response = self.client.get(reverse("main:show_experience"))
-
         self.assertContains(response, "Belum ada pengalaman yang ditambahkan.")
+        
+    def test_artwork_page(self):
+        response = self.client.get(reverse("main:show_artworks"))
+        
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "artworks.html")
+        self.assertContains(response, self.artwork.title)
+        self.assertContains(response, self.artwork.art_image)
+        self.assertContains(response, f'href="{reverse("main:show_main")}"')
+        
+    def test_empty_artwork_page(self):
+        Artwork.objects.all().delete()
+        response = self.client.get(reverse("main:show_artworks"))
+        self.assertContains(response, "No Artworks Submitted Yet.")
 
     def test_completed_experience(self):
         self.experience.ended_at = timezone.now()
