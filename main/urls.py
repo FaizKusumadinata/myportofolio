84f246ea84947246e283e883be6d68a0d16990a4
Faiz Kusumadinata <faizkusumadinata12@gmail.com>
2026-09-14 14:53:32 +0700

    Tutorial 3

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_artworks, show_art_details
+from main.views import show_main, show_experience, show_artworks, show_art_details, create_project, show_projects, get_projects_json, delete_project
 
 app_name = "main"
 
@@ -9,4 +9,8 @@ urlpatterns = [
     path("experience/", show_experience, name="show_experience"),
     path("artworks/", show_artworks, name="show_artworks"),
     path("art_details/<str:id>/", show_art_details, name="show_art_details"),
+    path("projects/add/", create_project, name="create_project"),
+    path("projects/", show_projects, name="show_projects"),
+    path("api/projects/", get_projects_json, name="get_projects_json"),
+    path("projects/<uuid:project_id>/delete/",delete_project,name="delete_project")
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-
-# Create your views here.
-from django.shortcuts import render
-
-from main.models import Experience, Artwork
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core import serializers
+from django.http import HttpResponse
+from django.contrib import messages
+from main.models import Experience, Artwork, Project
+from main.forms import ProjectForm
 
 
 def show_main(request):
@@ -40,4 +40,54 @@ def show_art_details(request, id):
         "art_detail":art_detail,
     }
     return render(request, "art_details.html", context)
+
+def create_project(request):
+    form = ProjectForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Proyek baru berhasil ditambahkan!")
+        return redirect("main:show_projects")
+
+    context = {
+        "name": "Burhan",
+        "form": form,
+    }
+    return render(request, "projects_form.html", context)
+
+def show_projects(request):
+    json_response = get_projects_json(request)
+
+    projects = serializers.deserialize(
+        "json",
+        json_response.content.decode("utf-8"),
+    )
+    projects = [project.object for project in projects]
+    title_query = request.GET.get("title", "").strip()
+
+    context = {
+        "name": "Burhan",
+        "project_list": projects,
+        "title_query": title_query,
+    }
+    return render(request, "projects.html", context)
+
+def get_projects_json(request):
+    title_query = request.GET.get("title", "").strip()
+    projects = Project.objects.all()
+
+    if title_query:
+        projects = projects.filter(title__icontains=title_query)
+
+    projects_json = serializers.serialize("json", projects)
+    return HttpResponse(projects_json, content_type="application/json")
     
+def delete_project(request, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+
+    if request.method == "POST":
+        project.delete()
+        messages.success(request, "Project berhasil dihapus!")
+        return redirect("main:show_projects")
+
+    return redirect("main:show_projects")
